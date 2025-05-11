1b8ed26e1c4ac1989a09192fb9b3e07726568e0a
sunnybird <jinlongfeng@live.com>
2025-05-12 06:29:36 +0800

    add image resize

--- nv21-sample/display_image.cpp
+++ nv21-sample/display_image.cpp
@@ -1,12 +1,10 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
 #include <opencv2/opencv.hpp>
 
 // 裁剪 NV21 数据
-bool cropNV21(const cv::Mat& nv21, cv::Mat& croppedNv21,
-              int width, int height,
-              int cropX, int cropY, int cropWidth, int cropHeight,
-              const std::string& debugDir,
+bool cropNV21(const cv::Mat& nv21, cv::Mat& croppedNv21, int width, int height, int cropX,
+              int cropY, int cropWidth, int cropHeight, const std::string& debugDir,
               bool showDebugWindows)
 {
     if (nv21.empty() || nv21.rows != height * 3 / 2 || nv21.cols != width) {
@@ -31,7 +29,7 @@ bool cropNV21(const cv::Mat& nv21, cv::Mat& croppedNv21,
 
     // Draw rectangle for visualization
     cv::Mat bgrWithRoi = bgr.clone();
-    cv::rectangle(bgrWithRoi, roi, cv::Scalar(0, 0, 255), 2); // red box
+    cv::rectangle(bgrWithRoi, roi, cv::Scalar(0, 0, 255), 2);   // red box
 
     // Debug: save images
     if (!debugDir.empty()) {
@@ -61,14 +59,14 @@ bool cropNV21(const cv::Mat& nv21, cv::Mat& croppedNv21,
     cv::Mat yuvI420;
     cv::cvtColor(croppedBGR, yuvI420, cv::COLOR_BGR2YUV_I420);
 
-    int ySize = cropWidth * cropHeight;
-    int uvSize = ySize / 4;
+    int          ySize  = cropWidth * cropHeight;
+    int          uvSize = ySize / 4;
     const uchar* yPlane = yuvI420.data;
     const uchar* uPlane = yuvI420.data + ySize;
     const uchar* vPlane = yuvI420.data + ySize + uvSize;
 
     croppedNv21.create(cropHeight * 3 / 2, cropWidth, CV_8UC1);
-    uchar* dstY = croppedNv21.data;
+    uchar* dstY  = croppedNv21.data;
     uchar* dstVU = dstY + ySize;
 
     memcpy(dstY, yPlane, ySize);
@@ -81,14 +79,24 @@ bool cropNV21(const cv::Mat& nv21, cv::Mat& croppedNv21,
 }
 
 // Function to convert NV21 to BGR format
-cv::Mat nv21ToBGR(const unsigned char *nv21Data, int width, int height) {
+cv::Mat nv21ToBGR(const unsigned char* nv21Data, int width, int height)
+{
     // Create a YUV420sp Mat (NV21 format)
-    cv::Mat nv21(height + height / 2, width, CV_8UC1, const_cast<unsigned char *>(nv21Data));
+    cv::Mat nv21(height + height / 2, width, CV_8UC1, const_cast<unsigned char*>(nv21Data));
 
     int cropX = 50, cropY = 50, cropWidth = 300, cropHeight = 400;
     // 裁剪 NV21 数据
     cv::Mat croppedNv21;
-    if (!cropNV21(nv21, croppedNv21, width, height, cropX, cropY, cropWidth, cropHeight,"../data/",true)) {
+    if (!cropNV21(nv21,
+                  croppedNv21,
+                  width,
+                  height,
+                  cropX,
+                  cropY,
+                  cropWidth,
+                  cropHeight,
+                  "../data/",
+                  true)) {
         // return -1;
     }
 
@@ -101,11 +109,12 @@ cv::Mat nv21ToBGR(const unsigned char *nv21Data, int width, int height) {
 }
 
 // 将 I420 格式转换为 NV21 格式
-void I420ToNV21(const uint8_t* i420, uint8_t* nv21, int width, int height) {
-    int frameSize = width * height;
-    const uint8_t* yPlane = i420;
-    const uint8_t* uPlane = i420 + frameSize;
-    const uint8_t* vPlane = i420 + frameSize + (frameSize / 4);
+void I420ToNV21(const uint8_t* i420, uint8_t* nv21, int width, int height)
+{
+    int            frameSize = width * height;
+    const uint8_t* yPlane    = i420;
+    const uint8_t* uPlane    = i420 + frameSize;
+    const uint8_t* vPlane    = i420 + frameSize + (frameSize / 4);
 
     // 复制 Y 分量
     memcpy(nv21, yPlane, frameSize);
@@ -113,64 +122,86 @@ void I420ToNV21(const uint8_t* i420, uint8_t* nv21, int width, int height) {
     // 交错 VU
     uint8_t* vuPlane = nv21 + frameSize;
     for (int i = 0; i < frameSize / 4; ++i) {
-        *vuPlane++ = vPlane[i]; // V
-        *vuPlane++ = uPlane[i]; // U
+        *vuPlane++ = vPlane[i];   // V
+        *vuPlane++ = uPlane[i];   // U
     }
 }
 
-std::string removeFileExtension(const std::string& filePath) {
+std::string removeFileExtension(const std::string& filePath)
+{
     size_t lastDot = filePath.find_last_of('.');
     if (lastDot == std::string::npos) {
-        return filePath; // 没有扩展名
+        return filePath;   // 没有扩展名
     }
     return filePath.substr(0, lastDot);
 }
 
-std::string getFileExtension(const std::string& filePath) {
+std::string getFileExtension(const std::string& filePath)
+{
     size_t lastDot = filePath.find_last_of('.');
     if (lastDot == std::string::npos) {
-        return ""; // 没有扩展名
+        return "";   // 没有扩展名
     }
-    return filePath.substr(lastDot+1); // 包含点的扩展名
+    return filePath.substr(lastDot + 1);   // 包含点的扩展名
 }
 
-std::string convertbgr2yuv(std::string bgrFilePath) {
+std::string convertbgr2yuv(std::string bgrFilePath)
+{
     cv::Mat bgrImage = cv::imread(bgrFilePath, cv::IMREAD_COLOR);
     if (bgrImage.empty()) {
         std::cerr << "无法读取图片: " << bgrFilePath << std::endl;
         return std::string("");
     }
-    int width = bgrImage.cols;
-    int height = bgrImage.rows;
+    int width  = 640;
+    int height = 480;
+
+    std::string filename      = removeFileExtension(bgrFilePath);
+    std::string fileExtension = getFileExtension(bgrFilePath);
+
+    cv::Mat bgrImage_640x480;
+    bgrImage_640x480 = cv::Mat::zeros(height, width, CV_8UC3);
+    cv::resize(bgrImage, bgrImage_640x480, bgrImage_640x480.size());
+    std::string outputPath = filename.append("_")
+                                 .append(std::to_string(width))
+                                 .append("x")
+                                 .append(std::to_string(height))
+                                 .append(".")
+                                 .append(fileExtension);
+    cv::imwrite(outputPath, bgrImage_640x480);
+    std::cout << "outputPath: " << outputPath << std::endl;
 
     // 转换为 YUV I420 格式
     cv::Mat yuvI420;
-    cv::cvtColor(bgrImage, yuvI420, cv::COLOR_BGR2YUV_I420);
+    cv::cvtColor(bgrImage_640x480, yuvI420, cv::COLOR_BGR2YUV_I420);
 
     // 分配 NV21 数据缓冲区
-    long nv21Size = width * height * 3 / 2;
+    long                 nv21Size = width * height * 3 / 2;
     std::vector<uint8_t> nv21Data(nv21Size);
 
     I420ToNV21(yuvI420.data, nv21Data.data(), width, height);
 
-    std::string outputPath = removeFileExtension(bgrFilePath)
-    .append("_").append(std::to_string(width)).append("x").append(std::to_string(height))
-    .append(".nv21");
+    outputPath = removeFileExtension(bgrFilePath)
+                     .append("_")
+                     .append(std::to_string(width))
+                     .append("x")
+                     .append(std::to_string(height))
+                     .append(".nv21");
 
     // 保存 NV21 到文件
     std::ofstream outFile(outputPath, std::ios::binary);
     outFile.write(reinterpret_cast<char*>(nv21Data.data()), nv21Size);
     outFile.close();
 
-    std::cout <<"转换完成，保存到: " << outputPath << std::endl;
+    std::cout << "转换完成，保存到: " << outputPath << std::endl;
 
     return outputPath;
 }
 
 // xxx_640x480.nv21
-void displayNV21File(std::string& nv21FilePath) {
-    int width= 0;
-    int height =0;
+void displayNV21File(std::string& nv21FilePath)
+{
+    int width  = 0;
+    int height = 0;
 
     std::string ext = getFileExtension(nv21FilePath);
     if (ext != "nv21") {
@@ -179,13 +210,12 @@ void displayNV21File(std::string& nv21FilePath) {
     }
 
     std::string filename = removeFileExtension(nv21FilePath);
-    std::string sizestr= filename.substr(filename.find_last_of('_') + 1, filename.length());
+    std::string sizestr  = filename.substr(filename.find_last_of('_') + 1, filename.length());
 
-    width = atoi(sizestr.substr(0,sizestr.find_last_of('x')).c_str());
+    width  = atoi(sizestr.substr(0, sizestr.find_last_of('x')).c_str());
     height = atoi(sizestr.substr(sizestr.find_last_of('x') + 1, sizestr.length()).c_str());
 
-
-    long nv21Size = width * height * 3 / 2;
+    long                 nv21Size = width * height * 3 / 2;
     std::vector<uint8_t> nv21Data(nv21Size);
 
     std::ifstream inputFile(nv21FilePath, std::ios::binary);
@@ -196,34 +226,39 @@ void displayNV21File(std::string& nv21FilePath) {
     cv::Mat bgrImage = nv21ToBGR(nv21Data.data(), width, height);
 
     std::string outputPath = removeFileExtension(nv21FilePath);
-    std::string outputPng =outputPath.substr(0,outputPath.find_last_of("_")).append("_out").append(".png");
+    std::string outputPng =
+        outputPath.substr(0, outputPath.find_last_of("_")).append("_out").append(".png");
     // 保存为 PNG
     if (!cv::imwrite(outputPng, bgrImage)) {
         std::cerr << "无法保存 PNG 文件: " << outputPng << std::endl;
-    }else {
+    }
+    else {
         std::cout << "保存 PNG 文件: " << outputPng << std::endl;
     }
 
     // Display the BGR image
     cv::imshow("NV21 Image", bgrImage);
-
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[])
+{
 
     if (argc < 2) {
         std::cerr << "Usage: " << argv[0] << " <file_path>" << std::endl;
-        std::cerr << "file_path supported: \n" << " xxx.png \n xxx.jpg \n xxx_widthxheight.nv21 " << std::endl;
+        std::cerr << "file_path supported: \n"
+                  << " xxx.png \n xxx.jpg \n xxx_widthxheight.nv21 " << std::endl;
         return -1;
     }
 
     std::string inputPath = argv[1];
-    std::string ext = getFileExtension(inputPath);
+    std::string ext       = getFileExtension(inputPath);
     std::string nv21Path;
     if (ext == "jpg" || ext == "png") {
-        std::cout << "开始转换: " << ext << "---->" << "nv21" << std::endl;
-       nv21Path= convertbgr2yuv(inputPath);
-    }else if (ext == "nv21") {
+        std::cout << "开始转换: " << ext << "---->"
+                  << "nv21" << std::endl;
+        nv21Path = convertbgr2yuv(inputPath);
+    }
+    else if (ext == "nv21") {
         nv21Path = inputPath;
     }
 
--- nv21-sample/nv21_affine_dpseek.c
+++ nv21-sample/nv21_affine_dpseek.c
@@ -1,40 +1,45 @@
-#include <stdint.h>
 #include <math.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #define CLAMP(v, min, max) ((v) < (min) ? (min) : ((v) > (max) ? (max) : (v)))
 
-typedef struct {
-    uint8_t *y; // 亮度分量
-    uint8_t *vu; // 色度分量(VU交错)
-    int width;
-    int height;
+typedef struct
+{
+    uint8_t* y;    // 亮度分量
+    uint8_t* vu;   // 色度分量(VU交错)
+    int      width;
+    int      height;
 } NV21Image;
 
-typedef struct {
-    float a, b, c; // 线性变换参数
-    float d, e, f; // 平移参数
+typedef struct
+{
+    float a, b, c;   // 线性变换参数
+    float d, e, f;   // 平移参数
 } AffineMatrix;
 
-NV21Image *create_nv21(int w, int h) {
-    NV21Image *img = malloc(sizeof(NV21Image));
-    img->y = malloc(w * h);
-    img->vu = malloc(w * h / 2);
-    img->width = w;
-    img->height = h;
+NV21Image* create_nv21(int w, int h)
+{
+    NV21Image* img = malloc(sizeof(NV21Image));
+    img->y         = malloc(w * h);
+    img->vu        = malloc(w * h / 2);
+    img->width     = w;
+    img->height    = h;
     return img;
 }
 
-void free_nv21(NV21Image *img) {
+void free_nv21(NV21Image* img)
+{
     free(img->y);
     free(img->vu);
     free(img);
 }
 
-int read_nv21_file(NV21Image *img, const char *filename) {
-    FILE *fp = fopen(filename, "rb");
+int read_nv21_file(NV21Image* img, const char* filename)
+{
+    FILE* fp = fopen(filename, "rb");
     if (fp != NULL) {
         fread(img->y, img->width * img->height, 1, fp);
         fread(img->vu, img->width * img->height / 2, 1, fp);
@@ -45,8 +50,9 @@ int read_nv21_file(NV21Image *img, const char *filename) {
     return -1;
 }
 
-int write_nv21_file(NV21Image *img, const char *filename) {
-    FILE *fp = fopen(filename, "wb");
+int write_nv21_file(NV21Image* img, const char* filename)
+{
+    FILE* fp = fopen(filename, "wb");
     if (fp) {
         fwrite(img->y, img->width * img->height, 1, fp);
         fwrite(img->vu, img->width * img->height / 2, 1, fp);
@@ -57,53 +63,52 @@ int write_nv21_file(NV21Image *img, const char *filename) {
     return -1;
 }
 
-AffineMatrix matrix_multiply(AffineMatrix m1, AffineMatrix m2) {
-    return (AffineMatrix){
-        .a = m1.a * m2.a + m1.b * m2.d,
-        .b = m1.a * m2.b + m1.b * m2.e,
-        .c = m1.a * m2.c + m1.b * m2.f + m1.c,
-        .d = m1.d * m2.a + m1.e * m2.d,
-        .e = m1.d * m2.b + m1.e * m2.e,
-        .f = m1.d * m2.c + m1.e * m2.f + m1.f
-    };
+AffineMatrix matrix_multiply(AffineMatrix m1, AffineMatrix m2)
+{
+    return (AffineMatrix){.a = m1.a * m2.a + m1.b * m2.d,
+                          .b = m1.a * m2.b + m1.b * m2.e,
+                          .c = m1.a * m2.c + m1.b * m2.f + m1.c,
+                          .d = m1.d * m2.a + m1.e * m2.d,
+                          .e = m1.d * m2.b + m1.e * m2.e,
+                          .f = m1.d * m2.c + m1.e * m2.f + m1.f};
 }
 
-AffineMatrix create_rotation_matrix(float theta) {
+AffineMatrix create_rotation_matrix(float theta)
+{
     float rad = theta * M_PI / 180.0f;
-    return (AffineMatrix){
-        cos(rad), -sin(rad), 0,
-        sin(rad), cos(rad), 0
-    };
+    return (AffineMatrix){cos(rad), -sin(rad), 0, sin(rad), cos(rad), 0};
 }
 
-AffineMatrix create_translation_matrix(float tx, float ty) {
+AffineMatrix create_translation_matrix(float tx, float ty)
+{
     return (AffineMatrix){1, 0, tx, 0, 1, ty};
 }
 
-AffineMatrix create_scale_matrix(float sx, float sy) {
+AffineMatrix create_scale_matrix(float sx, float sy)
+{
     return (AffineMatrix){sx, 0, 0, 0, sy, 0};
 }
 
-AffineMatrix create_centered_scale(float sx, float sy, int w, int h) {
-    AffineMatrix move_back = create_translation_matrix(-w / 2, -h / 2);
-    AffineMatrix scale = create_scale_matrix(sx, sy);
+AffineMatrix create_centered_scale(float sx, float sy, int w, int h)
+{
+    AffineMatrix move_back  = create_translation_matrix(-w / 2, -h / 2);
+    AffineMatrix scale      = create_scale_matrix(sx, sy);
     AffineMatrix move_front = create_translation_matrix(w / 2, h / 2);
     return matrix_multiply(move_front, matrix_multiply(scale, move_back));
 }
 
 // 裁剪 NV21 图像
-NV21Image* crop_nv21(const NV21Image* src, int left, int top, int crop_w, int crop_h) {
+NV21Image* crop_nv21(const NV21Image* src, int left, int top, int crop_w, int crop_h)
+{
     // 检查裁剪范围是否合法
-    if (left < 0 || top < 0 ||
-        left + crop_w > src->width ||
-        top + crop_h > src->height) {
+    if (left < 0 || top < 0 || left + crop_w > src->width || top + crop_h > src->height) {
         printf("裁剪范围超出源图像范围！\n");
         return NULL;
-        }
+    }
 
     // 强制坐标和尺寸为偶数（关键步骤）
-    left = (left / 2) * 2;
-    top = (top / 2) * 2;
+    left   = (left / 2) * 2;
+    top    = (top / 2) * 2;
     crop_w = (crop_w / 2) * 2;
     crop_h = (crop_h / 2) * 2;
 
@@ -117,82 +122,80 @@ NV21Image* crop_nv21(const NV21Image* src, int left, int top, int crop_w, int cr
 
     // 裁剪 Y 平面
     for (int row = 0; row < crop_h; row++) {
-        memcpy(cropped->y + row * crop_w,
-               src->y + (top + row) * src->width + left,
-               crop_w);
+        memcpy(cropped->y + row * crop_w, src->y + (top + row) * src->width + left, crop_w);
     }
 
     // 裁剪 VU 平面
     for (int row = 0; row < crop_h / 2; row++) {
-        memcpy(cropped->vu + row * crop_w,
-               src->vu + ((top / 2) + row) * src->width + left,
-               crop_w);
+        memcpy(cropped->vu + row * crop_w, src->vu + ((top / 2) + row) * src->width + left, crop_w);
     }
 
     return cropped;
 }
 
 // 镜像处理[6,8](@ref)
-void mirror_nv21(NV21Image* img) {
+void mirror_nv21(NV21Image* img)
+{
     // Y分量镜像
-    for(int y=0; y<img->height; y++) {
-        uint8_t* row = img->y + y*img->width;
-        for(int left=0, right=img->width-1; left<right; left++, right--) {
+    for (int y = 0; y < img->height; y++) {
+        uint8_t* row = img->y + y * img->width;
+        for (int left = 0, right = img->width - 1; left < right; left++, right--) {
             uint8_t tmp = row[left];
-            row[left] = row[right];
-            row[right] = tmp;
+            row[left]   = row[right];
+            row[right]  = tmp;
         }
     }
 
     // UV分量镜像[8](@ref)
     uint8_t* uv = img->vu;
-    for(int y=0; y<img->height/2; y++) {
-        for(int left=0, right=img->width-2; left<right; left+=2, right-=2) {
+    for (int y = 0; y < img->height / 2; y++) {
+        for (int left = 0, right = img->width - 2; left < right; left += 2, right -= 2) {
             uint8_t tmp_v = uv[left];
-            uint8_t tmp_u = uv[left+1];
-            uv[left] = uv[right];
-            uv[left+1] = uv[right+1];
-            uv[right] = tmp_v;
-            uv[right+1] = tmp_u;
+            uint8_t tmp_u = uv[left + 1];
+            uv[left]      = uv[right];
+            uv[left + 1]  = uv[right + 1];
+            uv[right]     = tmp_v;
+            uv[right + 1] = tmp_u;
         }
         uv += img->width;
     }
 }
 
-uint8_t bilinear_interpolate_y(const NV21Image *src, float x, float y) {
-    int x0 = (int) x, y0 = (int) y;
-    int x1 = x0 + 1, y1 = y0 + 1;
+uint8_t bilinear_interpolate_y(const NV21Image* src, float x, float y)
+{
+    int   x0 = (int)x, y0 = (int)y;
+    int   x1 = x0 + 1, y1 = y0 + 1;
     float dx = x - x0, dy = y - y0;
 
-    x0 = CLAMP(x0, 0, src->width-1);
-    y0 = CLAMP(y0, 0, src->height-1);
-    x1 = CLAMP(x1, 0, src->width-1);
-    y1 = CLAMP(y1, 0, src->height-1);
+    x0 = CLAMP(x0, 0, src->width - 1);
+    y0 = CLAMP(y0, 0, src->height - 1);
+    x1 = CLAMP(x1, 0, src->width - 1);
+    y1 = CLAMP(y1, 0, src->height - 1);
 
     uint8_t v00 = src->y[y0 * src->width + x0];
     uint8_t v01 = src->y[y0 * src->width + x1];
     uint8_t v10 = src->y[y1 * src->width + x0];
     uint8_t v11 = src->y[y1 * src->width + x1];
 
-    return (uint8_t) (v00 * (1 - dx) * (1 - dy) +
-                      v01 * dx * (1 - dy) +
-                      v10 * (1 - dx) * dy +
-                      v11 * dx * dy);
+    return (uint8_t)(v00 * (1 - dx) * (1 - dy) + v01 * dx * (1 - dy) + v10 * (1 - dx) * dy +
+                     v11 * dx * dy);
 }
 
 
-void process_uv_component(uint8_t *dst_uv, const NV21Image *src, float x, float y) {
-    int src_x = CLAMP((int)(x/2 + 0.5f), 0, src->width/2 -1);
-    int src_y = CLAMP((int)(y/2 + 0.5f), 0, src->height/2 -1);
+void process_uv_component(uint8_t* dst_uv, const NV21Image* src, float x, float y)
+{
+    int src_x   = CLAMP((int)(x / 2 + 0.5f), 0, src->width / 2 - 1);
+    int src_y   = CLAMP((int)(y / 2 + 0.5f), 0, src->height / 2 - 1);
     int src_idx = src_y * (src->width / 2) * 2 + src_x * 2;
-    dst_uv[0] = src->vu[src_idx]; // V
-    dst_uv[1] = src->vu[src_idx + 1]; // U
+    dst_uv[0]   = src->vu[src_idx];       // V
+    dst_uv[1]   = src->vu[src_idx + 1];   // U
 }
 
 
-void affine_transform(NV21Image *dst, const NV21Image *src, AffineMatrix mat) {
-    memset(dst->y, 0, dst->width * dst->height); // 初始化为黑色背景
-    memset(dst->vu, 128, dst->width * dst->height / 2); // UV填充灰色
+void affine_transform(NV21Image* dst, const NV21Image* src, AffineMatrix mat)
+{
+    memset(dst->y, 0, dst->width * dst->height);          // 初始化为黑色背景
+    memset(dst->vu, 128, dst->width * dst->height / 2);   // UV填充灰色
 
     for (int y = 0; y < dst->height; y++) {
         for (int x = 0; x < dst->width; x++) {
@@ -215,11 +218,12 @@ void affine_transform(NV21Image *dst, const NV21Image *src, AffineMatrix mat) {
 
 
 // 示例主函数
-int main() {
+int main()
+{
     int ret = -1;
     // 创建测试图像(640x480)
-    NV21Image *src = create_nv21(640, 480);
-    NV21Image *dst = create_nv21(640, 480);
+    NV21Image* src = create_nv21(640, 480);
+    NV21Image* dst = create_nv21(640, 480);
 
     ret = read_nv21_file(src, "../input_640x480.nv21");
     if (ret != 0) {
@@ -230,9 +234,9 @@ int main() {
         // 创建30度旋转矩阵[2](@ref)
         // AffineMatrix mat =create_rotation_matrix(90.0f);
         // 创建复合变换矩阵：缩放1.5倍 + 平移(50,30) + 旋转30度
-        AffineMatrix scale = create_centered_scale(1.5f, 1.5f, src->width, src->height);
+        AffineMatrix scale     = create_centered_scale(1.5f, 1.5f, src->width, src->height);
         AffineMatrix translate = create_translation_matrix(50, 30);
-        AffineMatrix rotate = create_rotation_matrix(30.0f);
+        AffineMatrix rotate    = create_rotation_matrix(30.0f);
 
 
         // AffineMatrix combined = matrix_multiply(translate, matrix_multiply(rotate, scale));
@@ -251,7 +255,7 @@ int main() {
     }
 
     // 执行裁剪（左上角(100,50)，尺寸400x300）
-    NV21Image *dst2_cropped = crop_nv21(dst, 100, 50, 400, 300);
+    NV21Image* dst2_cropped = crop_nv21(dst, 100, 50, 400, 300);
     mirror_nv21(dst2_cropped);
 
     ret = write_nv21_file(dst2_cropped, "../output_400x300_dpseek_crop.nv21");
